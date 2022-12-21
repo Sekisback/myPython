3bb877075dc8608c89d106c1953cb84ebc1df219
sekisback <sek_is_back@gmx.de>
2022-12-21 10:10:22 +0100

    update

--- UservAbrechnung/main.py
+++ UservAbrechnung/main.py
@@ -10,9 +10,17 @@ import os
 # C:\Users\skornber\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\pyinstaller.exe
 # C:\Users\skornber\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\auto-py-to-exe
 
-
-customers = ["--- Kunden wählen ---", "SKA", "ZVO", "STD", "SWQ", "BBS", "SWNO",
-             "TOR"]
+# empty list
+customers = []
+# first dropdown selection
+cus_0 = ["--- Kunden wählen ---"]
+# ! normal invoice customer
+cus_1 = ["SKA", "ZVO", "STD", "TOR", "SWQ", "BBS", "KAKI", "EWS", "GEW", "OHS",
+         "SWNO", "SNF", "SSN", "SNY"]
+# ! foreign system customer
+cus_2 = ["EMD", "SWN"]
+# !other customer
+cus_3 = ["SHH"]
 
 workflows = ["Transfer AG",
              "Transfer an AG",
@@ -37,8 +45,6 @@ abort_codes = ["2000 - techn. Mangel",
                "112 - Haus abgerissen"
                ]
 results = {}
-
-save = True
 
 
 # ------------------------------------ GUI -----------------------------------
@@ -78,6 +84,28 @@ class GUI(customtkinter.CTk):
         self.treeview.column("key", minwidth=230, width=230, stretch=NO)
         self.treeview.column("value", minwidth=200, width=200, stretch=NO)
 
+        # MenuBar
+        self.menubar = Menu(self)
+        self.config(menu=self.menubar)
+        self.file_menu = Menu(self.menubar, tearoff=False,
+                              activebackground="#395E9C",
+                              activeforeground="#ffffff")
+
+        # MenuBar Menus
+        self.menubar.add_cascade(label="Datei", menu=self.file_menu)
+
+        # MenuBar Items
+        self.save = IntVar()  # // declare Variable as Integer
+        self.save.set(1)      # // set to default on
+        self.file_menu.add_checkbutton(label="Auswertung speichern",
+                                       variable=self.save,
+                                       onvalue=1, offvalue=0)
+        # MenuBar SeparatorLine
+        self.file_menu.add_separator()
+
+        self.file_menu.add_command(label="Beenden", command=self.quit)
+
+
 # ----------------------------------- PANDAS ----------------------------------
 
 
@@ -98,31 +126,38 @@ def open_dialog():
 
         get_file_path(file)
         get_file_name(file)
-
         template()
+
     except Exception as e:
-        messagebox.showerror("Woah!", f"Da ist ein Problem aufgetreten {e}")
+        pass
 
 
 def template():
     # Common query's
-    empty_row()
+    empty_row(1)
     periode()
     count_data()
 
     # Read customer from Combobox
     selection = str(window.combobox.get())
     # Normal Customer
-    if selection.startswith(("SKA", "ZVO")):
+    if selection in cus_1:
+        empty_row(2)
         finished()
+        failed_tour()
+        empty_row(3)
+        same_place()
+        empty_row(4)
+        meter_size()
     # Foreign System
-    elif selection.startswith(("BBS", "SWQ")):
+    elif selection in cus_2:
         foreign()
     # Other
-    elif selection.startswith(("STD", "SWNO")):
+    elif selection in cus_3:
         other()
 
     # Display results
+    print(results)
     show_results()
 
 
@@ -136,9 +171,9 @@ def get_file_name(file):
     filename = os.path.basename(file).split(".")[0]
 
 
-def empty_row():
-    # Make first Row empty
-    results[""] = ""
+def empty_row(i):
+    # Make empty Rows
+    results[(" " * i)] = ""
 
 
 def periode():
@@ -161,6 +196,56 @@ def finished():
 
     extension = ("_abgeschlossene_Aufträge")
     write_results(finished, extension)
+
+
+def failed_tour():
+    # show only lines with blank value
+    failed = df[
+        (df["ZählerNr"].isnull()) &
+        (df["Workflowschritt"].isin(workflows))
+    ]
+
+    found = (failed[
+        (failed["Kommentar"].str.contains(
+            '|'.join(abort_codes), na=False))])
+
+    extension = ("_Fehlfahrten_eindeutig")
+    write_results(found, extension)
+
+    not_found = (failed[
+        ~(failed["Kommentar"].str.contains(
+            '|'.join(abort_codes), na=False))])
+
+    extension = ("_Fehlfahrten_prüfen")
+    write_results(not_found, extension)
+
+    results["Fehlfahrten"] = len(found)
+    results["Fehlfahrten prüfen"] = len(not_found)
+
+
+def same_place():
+    # avoid panda warning
+    pd.options.mode.chained_assignment = None
+
+    finished = df[(df["Workflowschritt"].isin(workflows))]
+
+    finished["tmp_same"] = finished["Strasse"].str.cat(finished["HausNr"])
+    even = finished[finished.duplicated(subset=["tmp_same"], keep=False)]
+    even = even.loc[:, even.columns != "tmp_same"]
+
+    extension = ("_gleiche_Abnahmestelle")
+    write_results(even, extension)
+
+    results["Einzelne Abnahmestelle"] = (len(finished) - len(even))
+    results["Gleiche Abnahmestelle"] = len(even)
+
+
+def meter_size():
+    m_size = pd.DataFrame(
+        df.loc[(df["Workflowschritt"].isin(workflows)) &
+               (df["ZählerNr"].notnull())], columns=["Zählergröße"])
+    results.update(
+        m_size["Zählergröße"].value_counts().sort_index().to_dict())
 
 
 def foreign():
@@ -186,12 +271,15 @@ def show_results():
 
 
 def write_results(data, extension):
-    # Write excel file without index
-    if (save):
-        data.to_excel(path + "/" + filename + extension + ".xlsx", index=False)
+    writer = pd.ExcelWriter(path + "/" + filename + "_Auswerrtung" + ".xlsx",
+                            engine='xlsxwriter')
+    data.to_excel(writer, sheet_name=extension, index=False)
+    writer.save()
 
 
 if __name__ == "__main__":
+    # Merge customer lists
+    customers = cus_0 + cus_1 + cus_2 + cus_3
     window = GUI()
     window.widgets()
     window.mainloop()
